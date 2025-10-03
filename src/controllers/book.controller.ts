0a4596b4d1fc579f9bb0e881ba18028f81d89a83
arjunsaxena122 <arjunsaxena122@gmail.com>
2025-10-04 00:53:36 +0530

    ADD: make all book api controllers

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -53,3 +53,121 @@ export const addBooksByAdmin = async (req: IRequestUser, res: Response) => {
 
     return new ApiResponse(201, `${book_title} book is add successfully`)
 }
+
+export const updateBookByAdmin = async (req: IRequestUser, res: Response) => {
+    const { bookId } = req.params
+
+    if (!bookId) {
+        throw new ApiError(404, "requested bookID not found")
+    }
+
+    const book = await prisma.book.findUnique({
+        where: {
+            id: String(bookId)
+        }
+    })
+
+    if (!book) {
+        throw new ApiError(404, "Your requested book is not found")
+    }
+
+    const { book_title, book_description, book_price, book_publish_date, book_language, book_publisher, book_pages, book_rating, book_category, book_author_name }: IBookType = req.body
+
+    if ([book_title, book_description, book_price, book_publish_date, book_language, book_publisher, book_pages, book_rating, book_category, book_author_name].some(f => f === "")) {
+        throw new ApiError(400, "Please fill all the required fields")
+    }
+
+    if (!req.user || !req.user.id) {
+        throw new ApiError(404, "requested userId not found")
+    }
+
+    const updatedBook = await prisma.book.update({
+        where: {
+            id: String(book.id)
+        },
+        data: {
+            book_title,
+            book_description,
+            book_price,
+            book_publish_date,
+            book_language,
+            book_publisher,
+            book_pages,
+            book_rating,
+            book_category,
+            book_author_name,
+            userId: req.user.id
+        }
+    })
+
+    if (!updatedBook) {
+        throw new ApiError(500, "Internal server error, Please try again to update details.")
+    }
+
+
+    return new ApiResponse(200, `your ${book.book_title} book is update successfully`, updatedBook)
+}
+
+export const removeBookByAdmin = async (req: IRequestUser, res: Response) => {
+    const { bookId } = req.params
+
+    if (!bookId) {
+        throw new ApiError(404, "requested book id not found")
+    }
+
+    const book = await prisma.book.findUnique({
+        where: {
+            id: String(bookId)
+        }
+    })
+
+    if (!book) {
+        throw new ApiError(404, "Your requested book is not found")
+    }
+
+    const deletedBook = await prisma.book.delete({
+        where: {
+            id: String(bookId)
+        }
+    })
+
+    if (!deletedBook) {
+        throw new ApiError(500, "Internal server error, Please try again to remove book")
+    }
+
+    return new ApiResponse(200, `your ${book.book_title} book is remove successfully`, deletedBook)
+
+}
+
+export const getAllBooks = async (req: IRequestUser, res: Response) => {
+
+    const book = await prisma.book.findMany()
+
+    if (!book) {
+        throw new ApiError(404, "No book available")
+    }
+
+    return new ApiResponse(200, "all books fetched successfully", book)
+}
+
+export const getBookById = async (req: IRequestUser, res: Response) => {
+    const { bookId } = req.params
+
+    if (!bookId) {
+        throw new ApiError(404, "requested book id not found")
+    }
+
+    const book = await prisma.book.findUnique({
+        where: {
+            id: String(bookId)
+        }
+    })
+
+    if (!book) {
+        throw new ApiError(404, "Your requested book is not found")
+    }
+
+    return new ApiResponse(200, `your ${book.book_title} book is available`, book)
+
+}
+
